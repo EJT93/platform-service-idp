8047f930ac83353d3077fb13fc1616a6fd774dbb
EJT93 <elijahtorrence@gmail.com>
2026-03-30 23:17:19 -0400

    style: terraform fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket          = "elijah-terraform-state"
+    bucket               = "elijah-terraform-state"
     workspace_key_prefix = "platform-service-idp"
-    key             = "terraform.tfstate"
-    region          = "us-east-2"
-    dynamodb_table  = "elijah-terraform-lock-table"   
-    encrypt        = true
+    key                  = "terraform.tfstate"
+    region               = "us-east-2"
+    dynamodb_table       = "elijah-terraform-lock-table"
+    encrypt              = true
   }
 }
